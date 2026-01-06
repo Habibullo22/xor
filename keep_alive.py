460195a080e174a6b8d5e4eacb09b278ed26b997
Habibullo22 <habibulloabidjonov555@gmail.com>
2026-01-06 23:52:38 +0500

    Refactor keep_alive.py for better structure
    Updated the Flask app to improve functionality and naming.

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
-def index():
-    return "Bot tirik!"
+def home():
+    return "Bot ishlayapti"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
